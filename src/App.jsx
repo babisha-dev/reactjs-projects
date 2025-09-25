c85d352ce3d21bdcc0d3cf078098daee5cccf241
babisha04 <babishariya@gmail.com>
2025-09-25 13:13:56 +0530

    My First React code

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+function MyButton(){
+  return( 
+  <button>Click Me</button>
+);
+}
+export default function MyApp(){
+  return(
+    <div>
+    <h1>My first tutorial</h1>
+    <MyButton/>
+       </div>
+  );
+}
