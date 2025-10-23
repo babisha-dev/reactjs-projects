ca4196f3923604a0889958f95eece7cc90824d31
babisha04 <babishariya@gmail.com>
2025-10-23 21:59:16 +0530

    Expression

--- src/Tutorials/App.jsx
+++ src/Tutorials/App.jsx
@@ -16,6 +16,20 @@ function MyButton(){
 function sum(k){
   return 2*k;
 }
+function Car() {
+  const myobj = {
+    name: "Fiat",
+    model: "500",
+    color: "white"
+  };
+  return (
+    <>
+      <h1>My car is a {myobj.color} {myobj.name} {myobj.model}</h1>
+    </>
+  );
+}
+
+
 export default function MyApp(){
   return(
     <div>
